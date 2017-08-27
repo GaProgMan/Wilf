48ce38b98c65225aeb8c5188c63848bdab05b4a5
Jamie Taylor <seppydude@gmail.com>
2017-08-27 23:17:51 +0100

    Added Api controllers to Api area

---
+++ Wilf.UI/Api/ApiController.cs
@@ -0,0 +1,19 @@
+﻿using Wilf.Facade.ApiFacades;
+
+namespace Wilf.UI.Api
+{
+    public class ApiController : BaseApiController
+    {
+        private readonly IBaseApiFacade _apiFacade;
+
+        public ApiController(IBaseApiFacade apiFacade)
+        {
+            _apiFacade = apiFacade;
+        }
+
+        public string Index()
+        {
+            return _apiFacade.ApiUseageString();
+        }
+    }
+}
---
+++ Wilf.UI/Api/BaseApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wilf.UI.Api
+{
+    public class BaseApiController : Controller
+    {
+        protected JsonResult ErrorResponse(string message = "Not Found")
+        {
+            return Json (new {
+                Success = false,
+                Result = message
+            });
+        }
+
+        protected JsonResult MessageResult(string message, bool success = true)
+        {
+            return Json(new {
+                Success = success,
+                Result = message
+            });
+        }
+    }
+}
