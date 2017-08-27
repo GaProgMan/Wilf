b0d87b6002cd9b60fb0499a144a04050722469d2
Jamie Taylor <seppydude@gmail.com>
2017-08-27 19:58:12 +0100

    Added DbSeeder Helper

---
+++ Wilf.Persistence/Helpers/DatabaseSeeder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Wilf.Entities;
+
+namespace Wilf.Persistence.Helpers
+{
+    public class DatabaseSeeder
+    {
+        private readonly IWilfContext _wilfContext;
+
+        public DatabaseSeeder(IWilfContext wilfContext)
+        {
+            _wilfContext = wilfContext;
+        }
+
+        public async Task<int> SeedSeasonEntries()
+        {
+            _wilfContext.Seasons.Add(
+                new Season
+                {
+                    Name = "Mash Season 1",
+                    SeasonNumber = 1,
+                    Descritpion = "The first season of Mash",
+                    Episodes = new Collection<Episode>()
+                });
+
+            return await _wilfContext.SaveChangesAsync();
+        }
+    }
+}
