655be56b4b5a1ea234f76ad445435398102c735d
Jamie Taylor <seppydude@gmail.com>
2017-08-27 19:57:53 +0100

    Added Db Seeder extention method

---
+++ Wilf.Persistence/WilfContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Wilf.Persistence.Helpers;
+
+namespace Wilf.Persistence
+{
+    public static class WilfContextExtensions
+    {
+        public static int SeedDatabase(this WilfContext wilfContext)
+        {
+            var seasonCount = default(int);
+
+            var dbSeeder = new DatabaseSeeder(wilfContext);
+
+            seasonCount = dbSeeder.SeedSeasonEntries().Result;
+
+            return seasonCount;
+        }
+    }
+}
