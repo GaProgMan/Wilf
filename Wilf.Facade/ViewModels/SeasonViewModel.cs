f71c9cb1d7b096125f29e6df53d203fc9beb08a9
Jamie Taylor <seppydude@gmail.com>
2017-08-27 19:57:12 +0100

    Created Season View model for Facade

---
+++ Wilf.Facade/ViewModels/SeasonViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Wilf.Entities;
+
+namespace Wilf.Facade.ViewModels
+{
+    public class SeasonViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int SeasonNumber { get; set; }
+        public int EpisodeCount { get; set; }
+
+        public SeasonViewModel(Season dbModel)
+        {
+            Name = dbModel.Name;
+            Description = dbModel.Descritpion;
+            SeasonNumber = dbModel.SeasonNumber;
+            EpisodeCount = dbModel.Episodes.Count;
+        }
+    }
+}
